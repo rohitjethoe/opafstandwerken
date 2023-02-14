63e39738d167be01058d4ba933ba9f16873deacb
rohitjethoe <rohitjethoe@proton.me>
2023-02-14 21:41:05 +0100

    URL veranderd.

--- src/components/AddPost.vue
+++ src/components/AddPost.vue
@@ -57,7 +57,7 @@ export default {
     },
     methods: {
         publishPost: async function () {
-            const response = await fetch('https://opafstandwerken-api-production-19e9.up.railway.app↗/api/VacancyItems', {
+            const response = await fetch('http://opafstandwerken-api-production-19e9.up.railway.app/api/VacancyItems', {
                 method: 'POST',
                 mode: 'no-cors',
                 headers: {
